add206fb3fc2aab115df72c9f7be46968f52872d
Alexandru <alxborcea@gmail.com>
2024-05-13 17:19:25 +0300

    initial

---
+++ librariamea.c
@@ -0,0 +1,4 @@
+#include "librariamea.h"
+
+//aici scriu functiile
+
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <librariamea.h>
+
+int main(){
+    
+    
+
+    return 0;
+}
